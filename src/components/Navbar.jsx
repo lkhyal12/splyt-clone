657e169e35041a8d136aea2d9c80635d4b8c4a9f
lkhyal <lkhyal.ab@gmail.com>
2026-01-23 21:32:43 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed left-0 top-0 z-50 md:p-9 p-3">
+      <img src="/images/nav-logo.svg" alt="nav logo" className="w-20 md:w-24" />
+    </div>
+  );
+};
+
+export default Navbar;
